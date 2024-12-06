9cec5e3d3837138bba47c2126bdd92fa3463380b
ncarmona99 <nicolascarmonarioseco@gmail.com>
2024-12-06 18:58:09 -0300

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  api_url: 'https://xgdujjqhurmzfxelhysh.supabase.co/rest/v1',
+  apiKeySupabase:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnZHVqanFodXJtemZ4ZWxoeXNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwNDI3ODIsImV4cCI6MjA0NDYxODc4Mn0.272XWKo0AOvzWwKXiuz28Cp3_QcePQHXQDJ9f4ND-jI',
 };
