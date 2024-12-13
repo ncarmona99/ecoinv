881dbb7bf495f25dec05ef28692c89909bf458de
ncarmona99 <n.carmona@duocuc.cl>
2024-12-13 09:08:37 -0300

    commit de prueba para hacer build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'eco_inv',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: 'b3a71cfb',
+      channel: 'Production',
+      autoUpdateMethod: 'none',
+      maxVersions: 3,
+    },
+  },
 };
 
 export default config;
