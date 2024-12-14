43068a2f02a2424ad92b3f2701cc84456c74992f
ncarmona99 <nicolascarmonarioseco@gmail.com>
2024-12-14 00:48:56 -0300

    Push definitivo entrega final

--- src/app/pages/crea-producto/crea-producto.page.ts
+++ src/app/pages/crea-producto/crea-producto.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductosService } from 'src/app/services/productos/productos.service';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController, LoadingController } from '@ionic/angular';
 import { CameraService } from 'src/app/services/camera/camera.service';
 
 @Component({
@@ -15,11 +15,14 @@ export class CreaProductoPage {
     stock: 0,
     imagen: '',
   };
+  isLoading = false;
 
   constructor(
     private productosService: ProductosService,
     private navCtrl: NavController,
-    private cameraService: CameraService
+    private cameraService: CameraService,
+    private toastController: ToastController,
+    private loadingController: LoadingController
   ) {}
 
   async tomarFoto() {
@@ -31,12 +34,32 @@ export class CreaProductoPage {
   }
 
   async crearProducto() {
+    this.isLoading = true;
+    const loading = await this.loadingController.create({
+      message: 'Agregando producto...',
+    });
+    await loading.present();
+
     try {
       await this.productosService.agregarProducto(this.producto);
       console.log('Producto agregado exitosamente');
-      this.navCtrl.navigateBack('/productos'); // Redirige a la lista de productos
+      this.presentToast('Producto agregado exitosamente', 'success');
+      this.navCtrl.navigateBack('/hub'); // Redirige a la lista de productos
     } catch (error) {
       console.error('Error al agregar producto:', error);
+      this.presentToast('Error al agregar producto', 'danger');
+    } finally {
+      this.isLoading = false;
+      loading.dismiss();
     }
+  }
+
+  async presentToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      color,
+    });
+    toast.present();
   }
 }
